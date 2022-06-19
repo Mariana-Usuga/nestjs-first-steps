39a18a6408fceedd22a205375b3d4c4c6223b226
Wilmar <wilmar.munoz@whiteprompt.com>
2022-06-19 17:52:29 -0500

    create endpoint clear names

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { NamesModule } from './modules/names/names.module';
 
 @Module({
-  imports: [],
+  imports: [
+    NamesModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule {}
